327133e38b85cacbe72ca4d76746cf2003e14ba3
r6920966-star <r6920966@gmail.com>
2026-04-23 01:29:27 +0600

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T19:29:37.771Z
-Commit: Add multi-language support
+Timestamp: 2026-04-22T19:29:41.800Z
+Commit: Resolve null pointer exception in user service
