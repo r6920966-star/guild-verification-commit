cf2bbd54a964747d9bcc183996f81703aa58fa9b
r6920966-star <r6920966@gmail.com>
2026-04-23 01:32:39 +0600

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T19:32:10.601Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-22T19:32:54.367Z
+Commit: Fix broken links in documentation
