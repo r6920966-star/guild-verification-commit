623ced3b647bb46af76b29f395dccd5cf82be652
r6920966-star <r6920966@gmail.com>
2026-04-23 01:29:45 +0600

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T19:29:21.247Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-04-22T19:30:00.281Z
+Commit: Fix broken links in documentation
