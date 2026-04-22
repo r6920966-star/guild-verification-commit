16e0698338ba9a1d59ac9f134c227caae5b0d941
r6920966-star <r6920966@gmail.com>
2026-04-23 01:29:49 +0600

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T19:30:00.281Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-04-22T19:30:04.324Z
+Commit: Fix CSS styling bug on mobile devices
