d5f680399a27ec0e86aa7f8d84ba5dfe2fb6000f
r6920966-star <r6920966@gmail.com>
2026-04-23 01:31:56 +0600

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-22T19:32:03.326Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-22T19:32:10.601Z
+Commit: Fix memory leak in event handlers
